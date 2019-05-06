6941876fbce9b2844147b4c33a9fd29c569a6db9
Félix Pérez Collado <fepecol@uoc.edu>
2019-05-06 21:10:43 +0200

    Instalar angular material + login

--- src/app/shared/states/app/actions/app.actions.ts
+++ src/app/shared/states/app/actions/app.actions.ts
@@ -1,2 +1,1 @@
 import { Action } from '@ngrx/store';
-/*
--- src/app/views/signin/signin.module.ts
+++ src/app/views/signin/signin.module.ts
@@ -3,10 +3,29 @@ import { NgModule } from '@angular/core';
 import { SigninRoutingModule } from './signin-routing.module';
 import { SigninComponent } from './signin.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRippleModule,
+  MatCheckboxModule,
+  MatCardModule
+} from '@angular/material';
+import { MyOwnCustomMaterialModuleModule } from 'src/app/shared/my-own-custom-material-module/my-own-custom-material-module.module';
 
 @NgModule({
   declarations: [SigninComponent],
-  imports: [SharedModule, SigninRoutingModule],
+  imports: [
+    SharedModule, 
+    SigninRoutingModule, 
+    MyOwnCustomMaterialModuleModule, 
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MatCheckboxModule,
+    MatCardModule
+  ],
   providers: []
 })
 export class SigninModule {}
