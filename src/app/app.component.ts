0ac9561250933d19e60fbd8b2bcea0e2faa64f91
Félix Pérez Collado <fepecol@uoc.edu>
2019-05-21 20:18:44 +0200

    Reemplazar codigo PEC4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @Output() public sidenavToggle = new EventEmitter();
-
-  title = 'UOCJob';
-
-  public onToggleSidenav = () => {
-    this.sidenavToggle.emit();
-  }
+  title = 'Tour of Heroes';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,42 @@
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './shared/core.module';
-import { RouterModule } from '@angular/router';
-import { rootRouterConfig } from './app-routing';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
+
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { FakeBackendService } from './shared/inmemory-db/fake-backend.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import {MaterialModule} from './shared/modules/material.module';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppRoutingModule }     from './app-routing.module';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+import { HeroesComponent }      from './heroes/heroes.component';
+import { HeroSearchComponent }  from './hero-search/hero-search.component';
+import { MessagesComponent }    from './messages/messages.component';
 
 @NgModule({
   imports: [
-    SharedModule,
-    CoreModule,
-    RouterModule.forRoot(rootRouterConfig, { useHash: false }),
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
     HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(FakeBackendService, {
-      dataEncapsulation: false
-    }),
-    BrowserAnimationsModule,
-    FlexLayoutModule,
-    MaterialModule
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
-  declarations: [AppComponent],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    HeroSearchComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule {}
+export class AppModule { }
